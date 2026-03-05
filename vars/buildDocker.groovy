1f3718a7781630ac1b53aac73999168e82a48f45
dewnuwan7 <dewnuwan@icloud.com>
2026-03-05 17:13:28 +0530

    added docker push

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -5,5 +5,6 @@ def call(String imageName){
         withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
             sh "docker build -t $imageName ."
             sh "echo $PASS | docker login -u $USER --password-stdin"
+            sh "docker push $imageName"
         }
 }
