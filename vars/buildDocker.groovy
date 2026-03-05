7c99e4ae7972f0b2f8a76022472d61508eefc19f
dewnuwan7 <dewnuwan@icloud.com>
2026-03-05 17:03:47 +0530

    added params for def call in docker image

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
      echo "building Docker image.."
         withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-            sh "docker build -t dewnuwan/java-maven-app:jma-${params.VERSION} ."
+            sh "docker build -t $imageName ."
             sh "echo $PASS | docker login -u $USER --password-stdin"
         }
 }
